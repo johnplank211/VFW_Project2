f15b35692ebdc1ba3f0903e1e5874136f9c943fe
John plank <johnplank211@gmail.com>
2012-05-10 10:07:22 -0400

    Last Commit

--- main.js
+++ main.js
@@ -1,46 +1,112 @@
 /*John Plank
 VFW Project 2
-05-05-2012
+term 06
 Apocalypse Checklist*/
 
-console.log("suck it world");
+window.addEventListener("DOMContentLoaded", function(){
+		var $ = function(x) {
+	    var theElement = document.getElementById(x);
+		return theElement;
+	};
 
-var $ = function (x) {
-	var theElemenent = document.getElementById(x)
-	return theElement;
-};
-
-var pw    = $("password");
-var cpw   = $("confirm");
-var check = $("submit");
-
-
-console.log("suck it more");
-
-
-var checkpw = function () {
-	console.log("pw checked");
-	if (pw.value != "") {
-		cpw.removeAttribute("disabled", "disabled");
+	var makeCats = function () {
+		var formTags = document.getElementsByTagName("form"),
+			selectLi = $("select"),
+			makeSelect = document.createElement("select");
+		makeSelect.setAttribute("id", "groups");
+		for (var i = 0, j = fearGroups.length; i<j; i++) {
+			var makeOption = document.createElement('option');
+			var optText = fearGroups[i];
+			makeOption.setAttribute('value', fearGroups[i]);
+			makeOption.innerHTML = optText;
+			makeSelect.appendChild(makeOption);
 	}
-};
+		selectLi.appendChild(makeSelect);
+	};
 
-console.log("suck it even more");
+	var fearGroups = ["--Fear level--", "In denial still", "Oops I crapped my pants", "Psalm 144:1"];
+	
+	var getRadio = function () {
+		var radio = document.forms[0].apocalypse;
+		for (var i = 0; i < radio.length; i++) {
+			if (radio[i].checked) {
+				apocalypseValue = radio[i].value;
+			}
 
- var comparePasswords = function () {
- 	if (pw.value === cpw.value) {
- 		alert("The passwords match");
- 	} else {
- 		alert("The passwords do not match. Please try agian. ")
- 	}
- };
+		}
+	};
 
+	function storeData() {
+		var id    			= Math.floor(Math.random()* 1000001);
+		getRadio();
+		var item 			= {};
+		    item.Apocalypse = ["Apocalypse:", apocalypseValue];
+			item.fear       = ["Fear level:", $("groups").value];
+			item.firearm	= ["Firearm:", $("firearm").value];
+			item.ammo		= ["Ammo:", $("ammo").value];
+			item.melee 		= ["Melee weapon:", $("meleeWeapon").value];
+			item.canned		= ["Canned:", $("cannedFood").value];
+			item.water		= ["Water:", $("water").value];
+			item.chain		= ["Chain mesh suit:", $("chainMeshSuit").value];
+			item.map 		= ["Topographical Map:", $("topographicalMap").value];
+			item.leatherman = ["Leatherman:", $("leatherman").value];
+			item.rucksack	= ["Rucksack:", $("rucksack").value];
+			item.boots		= ["Boots:", $("boots").value];
+			item.matches	= ["Matches:", $("matches").value];
+			item.p38		= ["P38:", $("p38").value];
+			item.intestinal = ["Intestinal Fortitude:", $("intestinalFortitude").value];
+			item.item		= ["Item:", $("item").value];			
+			item.date       = ["World Ended:", $("date").value];
+			item.email		= ["Email:", $("email").value];
+			item.comments	= ["Comments:", $("comments").value];
+			item.readiness  = ["Readiness:", $("readiness").value];
 
-pw.addEventListener("blur", checkpw);
-check.addEventListener("click", comparePasswords);
+		localStorage.setItem(id, JSON.stringify(item));
+		alert("Contact Saved!");
+	};
 
+	var getData = function () {
+		var makeDiv = document.createElement("div");
+		makeDiv.setAttribute("id", "items");
+		var makeList = document.createElement("ul");
+		makeDiv.appendChild(makeList);
+		document.body.appendChild(makeDiv);
+		for (var i = 0, len = localStorage.length; i < len; i++) {
+			 var makeli = document.createElement("li");
+			 makeList.appendChild(makeli);
+			 var key = localStorage.key(i);
+			 var value = localStorage.getItem(key);
+			 var obj = JSON.parse(value);
+			 var makeSubList = document.createElement("ul");
+			 makeli.appendChild(makeSubList);
+			 for (var t in obj) {
+			 	var makeSubLi = document.createElement("li");
+			 	makeSubList.appendChild(makeSubLi);
+			 	var optSubText = obj[t][0]+" "+obj[t][1];
+			 	makeSubLi.innerHTML = optSubText;
+			 }
+		}
+	};
 
+	var clearLocal = function () {
+		if (localStorage.length === 0) {
+			alert("All clear.")
+			}else{
+				localStorage.clear();
+				window.location.reload();
+				return false;
+		}
+	};
 
+		var apocalypseValue;
 
+	var displayLink = $("displayLink");
+		displayLink.addEventListener("click", getData);
+	 	var clearLink = $("clear");
+		clearLink.addEventListener("click", clearLocal);
+		var save = $("submit");
+		save.addEventListener("click", storeData);
 
+		makeCats();
 
+});
